aabd9daf3b11c2a1c2463e81a5b30bb79cc40163
xiaoli <1063080020@qq.com>
2023-11-30 20:57:26 +0800

    添加首页导航

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,41 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <header>
+    <div class="containner">
+      <div class="logo">
+        <a href="#">网易云音乐</a>
+      </div>
+      <ul>
+        <li>发现音乐</li>
+      </ul>
+    </div>
+  </header>
 </template>
+<style scoped lang="less">
+header {
+  height: 70px;
+  background-color: #242424;
+}
+
+.containner {
+  width: 1100px;
+  height: 70px;
+  /* background-color: red; */
+  margin: 0 auto;
+
+  .logo {
+    height: 69px;
+    width: 176px;
+    background: url(../assets/images/topbar.png) no-repeat;
+
+    a {
+      width: 157px;
+      height: 100%;
+      line-height: 70px;
+      opacity: 0;
+    }
+  }
+}
+</style>
