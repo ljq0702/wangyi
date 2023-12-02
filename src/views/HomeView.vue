83342756f47020b65c1cc13a44bb8f06e8ff45dc
xiaoli <1063080020@qq.com>
2023-12-02 17:14:52 +0800

    导航栏

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,4 +1,5 @@
 <script setup>
+  const nav = ['发现音乐','我的音乐','关注','商城', '音乐人','云推歌','下载客户端']
 </script>
 
 <template>
@@ -8,7 +9,7 @@
         <a href="#">网易云音乐</a>
       </div>
       <ul>
-        <li>发现音乐</li>
+        <li v-for="(item,index) in nav"><a href="#">{{ item }}</a></li>
       </ul>
     </div>
   </header>
@@ -24,17 +25,28 @@ header {
   height: 70px;
   /* background-color: red; */
   margin: 0 auto;
-
+  display: flex;
   .logo {
     height: 69px;
     width: 176px;
     background: url(../assets/images/topbar.png) no-repeat;
-
+    margin-right: 20px;
     a {
       width: 157px;
       height: 100%;
       line-height: 70px;
       opacity: 0;
+    }
+  }
+  ul{
+    display: flex;
+    align-items: center;
+    padding: 0;
+     li{
+      height: 70px;
+      line-height: 70px;
+      color: #fff;
+      padding: 0 20px;
     }
   }
 }
